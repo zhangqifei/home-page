2051bd2db1b03b68c49977016e48326dab592492
zhangqifei <1353250703@qq.com>
2018-04-12 19:30:38 +0800

    1

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/**
+ * Created by zhangqifei on 2017/4/21.
+ */
+
+(function () {
+
+    var bv = new Bideo();
+    bv.init({
+        // Video element
+        videoEl: document.querySelector('#background_video'),
+
+        // Container element
+        container: document.querySelector('body'),
+
+        // Resize
+        resize: true,
+
+        // autoplay: false,
+
+        isMobile: window.matchMedia('(max-width: 768px)').matches,
+
+        playButton: document.querySelector('#play'),
+        pauseButton: document.querySelector('#pause'),
+
+        // Array of objects containing the src and type
+        // of different video formats to add
+        src: [
+            {
+                src: 'css/highline.mp4',
+                type: 'video/mp4'
+            }
+        ],
+
+        // What to do once video loads (initial frame)
+        onLoad: function () {
+            document.querySelector('#video_cover').style.display = 'none';
+        }
+    });
+}());
